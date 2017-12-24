2c6c5daa5ac7b9cf9d1d5aa9488acfff3cf724b9
Aleksey Zolochevskiy <aleksey.zolochevski@gmail.com>
2017-12-24 15:42:49 +0200

    + Collapse object

--- tab/tab.js
+++ tab/tab.js
@@ -1,11 +1,31 @@
-z('[data-collapse]').on("click", function () {
-    var targetId = this.getAttribute("data-collapse");
+// z('[data-collapse]').on("click", function () {
+//
+// });
+
+function Collapse(buttonElement) {
+    var targetId = buttonElement.getAttribute("data-collapse");
     var zTarget = z('#' + targetId);
 
-    if (zTarget.isVisible()) {
-        zTarget.hide();
-    } else {
-        zTarget.show();
-    }
+    var obj = {
+        open: function () {
+            zTarget.show();
+        },
 
-});
+        close: function () {
+            zTarget.hide();
+        },
+
+        toggle: function () {
+            if (zTarget.isVisible()) {
+                zTarget.hide();
+            } else {
+                zTarget.show();
+            }
+        }
+    };
+
+    return obj;
+}
+
+var collapse1 = Collapse(z('button')[0]);
+var collapse2 = Collapse(z('button')[1]);
