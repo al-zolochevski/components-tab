686c60a4675868bf138499f875dbd6fc360182b8
Aleksey Zolochevskiy <aleksey.zolochevski@gmail.com>
2018-01-06 15:16:46 +0200

    + TODO:

--- tab/tab.js
+++ tab/tab.js
@@ -35,16 +35,23 @@
 // var collapse1 = Collapse(z('button')[0]); // TODO: new Collapse(...)
 // var collapse2 = Collapse(z('button')[1]);
 
+/* Alex L.:
+ * 1) конструктор
+ * 2) new Collapse()
+* */
 
 var collapse1 = {
     button: document.getElementById("button1"),
     target: document.getElementById("collapse1"),
+
     open: function () {
         z(this.target).show()
     },
     close: function () {
         z(this.target).hide()
     }
+
+    // TODO: toggle
 };
 
 z(collapse1.button).on("click", collapse1.open);
@@ -58,4 +65,6 @@ var collapse2 = {
     close: function () {
         z(this.target).hide()
     }
+
+    // TODO: toggle
 };
