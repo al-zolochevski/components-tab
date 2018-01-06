a75b429433d4581c1a9b506941aeb6db0b626c0f
Aleksey Zolochevskiy <aleksey.zolochevski@gmail.com>
2018-01-06 15:02:54 +0200

    + low level obj

--- tab/tab.js
+++ tab/tab.js
@@ -1,33 +1,59 @@
-function Collapse(buttonElement) {
-    var targetId = buttonElement.getAttribute("data-collapse");
-    var target = document.getElementById(targetId);
-    var zTarget = z(target);
-
-    return {
-        target: target,
-        zTarget: zTarget,
-
-        open: function () {
-            zTarget.show();
-        },
-
-        close: function () {
-            zTarget.hide();
-        },
-
-        toggle: function () {
-            if (zTarget.isVisible()) {
-                this.close();
-            } else {
-                this.open();
-            }
-        }
-    };
-}
-
-// TODO: resurrect unobtrusive JS
+// function Collapse(buttonElement) {
+//     var targetId = buttonElement.getAttribute("data-collapse");
+//     var target = document.getElementById(targetId);
+//     var zTarget = z(target);
+//
+//     return {
+//         target: target,
+//         zTarget: zTarget,
+//
+//         open: function () {
+//             zTarget.show();
+//         },
+//
+//         close: function () {
+//             zTarget.hide();
+//         },
+//
+//         toggle: function () {
+//             if (zTarget.isVisible()) {
+//                 this.close();
+//             } else {
+//                 this.open();
+//             }
+//         }
+//     };
+// }
+//
+// // TODO: resurrect unobtrusive JS
 // z('[data-collapse]').on("click", function () {
 //
+//
 // });
-var collapse1 = Collapse(z('button')[0]); // TODO: new Collapse(...)
-var collapse2 = Collapse(z('button')[1]);
+//
+//
+// var collapse1 = Collapse(z('button')[0]); // TODO: new Collapse(...)
+// var collapse2 = Collapse(z('button')[1]);
+
+
+var collapse1 = {
+    button: document.getElementById("button1"),
+    target: document.getElementById("collapse1"),
+    open: function () {
+        z(this.target).show()
+    },
+    close: function () {
+        z(this.target).hide()
+    }
+};
+
+var collapse2 = {
+    button: document.getElementById("button2"),
+    target: document.getElementById("collapse2"),
+    open: function () {
+        z(this.target).show()
+    },
+    close: function () {
+        z(this.target).hide()
+    }
+};
