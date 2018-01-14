8b87ffebd12a9adbeda2e891a474b965c2027ff2
Aleksey Zolochevskiy <aleksey.zolochevski@gmail.com>
2018-01-14 14:04:12 +0200

    + use ES6

--- tab/tab.js
+++ tab/tab.js
@@ -42,22 +42,43 @@
 * */
 
 
-var Collapse = function(button) {
+// var Collapse = function(button) {
+//
+//     this.button = button;
+//     this.target = document.getElementById(button.getAttribute('data-collapse'));
+//
+//     z(this.button).on("click", this.open.bind(this));
+// };
+//
+// Collapse.prototype.open = function () {
+//     z(this.target).show()
+// };
+//
+// Collapse.prototype.close = function () {
+//     z(this.target).hide()
+// };
+//
+//
+// var collapse1 = new Collapse(document.getElementById("button1"));
+// var collapse2 = new Collapse(document.getElementById("button2"));
 
-    this.button = button;
-    this.target = document.getElementById(button.getAttribute('data-collapse'));
+class Collapse {
 
-    z(this.button).on("click", this.open.bind(this));
-};
+    constructor (button) {
+        this.button = button;
+        this.target = document.getElementById(button.getAttribute('data-collapse'));
 
-Collapse.prototype.open = function () {
-    z(this.target).show()
-};
+        z(this.button).on("click", this.open.bind(this));
+    };
 
-Collapse.prototype.close = function () {
-    z(this.target).hide()
-};
+    open () {
+        z(this.target).show()
+    };
 
+    close () {
+        z(this.target).hide()
+    };
+}
 
 var collapse1 = new Collapse(document.getElementById("button1"));
 var collapse2 = new Collapse(document.getElementById("button2"));
